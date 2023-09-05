e8e9bea95abe15944ca9fc740c65a2d33380fc4f
Pedro Vinicius <pedin1149@outlook.com>
2023-09-05 14:39:02 -0300

    Order, Instant ISO 8601 pt2

---
+++ src/main/java/com/educandoweb/project/repositories/OrderRepository.java
@@ -0,0 +1,9 @@
+package com.educandoweb.project.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.educandoweb.project.entities.Order;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+
+}
