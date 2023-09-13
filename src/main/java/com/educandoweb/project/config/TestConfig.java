d576cba19f3da347eba503df148bac522b37fe75
Pedro Vinicius <pedin1149@outlook.com>
2023-09-13 06:44:59 -0300

    Criada classe Payment com associação one-to-one

--- src/main/java/com/educandoweb/project/config/TestConfig.java
+++ src/main/java/com/educandoweb/project/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.educandoweb.project.entities.Category;
 import com.educandoweb.project.entities.Order;
 import com.educandoweb.project.entities.OrderItem;
+import com.educandoweb.project.entities.Payment;
 import com.educandoweb.project.entities.Product;
 import com.educandoweb.project.entities.User;
 import com.educandoweb.project.entities.enums.OrderStatus;
@@ -92,6 +93,10 @@ public class TestConfig implements CommandLineRunner {
 	p5.getCategories().add(cat2);
 	
 	productRepository.saveAll(Arrays.asList(p1,p2,p3,p4,p5));
+	
+	Payment pay1 = new Payment(null, Instant.parse("2019-06-20T22:53:07Z"), o1);
+	o1.setPayment(pay1);
+	orderRepository.save(o1);
 	}
 	
 }
