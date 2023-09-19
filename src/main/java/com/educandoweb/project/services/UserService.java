2277a06073203a043024daaadad6064b416668a2
Pedro Vinicius <pedin1149@outlook.com>
2023-09-19 14:29:53 -0300

    Tratamento de exceção - Delete

--- src/main/java/com/educandoweb/project/services/UserService.java
+++ src/main/java/com/educandoweb/project/services/UserService.java
@@ -4,47 +4,56 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.educandoweb.project.entities.User;
 import com.educandoweb.project.repositories.UserRepository;
+import com.educandoweb.project.services.exceptions.DatabaseException;
 import com.educandoweb.project.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
 
 	@Autowired
-	private UserRepository repository;
+	private UserRepository userRepository;
 
 	public List<User> findAll() {
-		return repository.findAll();
+		return userRepository.findAll();
 
 	}
 
 	public User findById(Long id) {
-		Optional<User> obj = repository.findById(id);
+		Optional<User> obj = userRepository.findById(id);
 		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 
 	public User insert(User obj) {
-		return repository.save(obj);
+		return userRepository.save(obj);
 	}
 
 	public void delete(Long id) {
-    repository.deleteById(id);
+		try {
+			User user = findById(id);
+			userRepository.delete(user);
+		} catch (ResourceNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
-public User update(Long id, User obj) {
-	User entity = repository.getReferenceById(id);
-	updateData(entity, obj);
-	return repository.save(entity);
-}
 
-private void updateData(User entity, User obj) {
-	entity.setName(obj.getName());
-	entity.setEmail(obj.getEmail());
-	entity.setPhone(obj.getPhone());
-	
-	
-}
+	public User update(Long id, User obj) {
+		User entity = userRepository.getReferenceById(id);
+		updateData(entity, obj);
+		return userRepository.save(entity);
+	}
+
+	private void updateData(User entity, User obj) {
+		entity.setName(obj.getName());
+		entity.setEmail(obj.getEmail());
+		entity.setPhone(obj.getPhone());
+
+	}
 
 }
